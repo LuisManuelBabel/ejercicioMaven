5a2719715c4478f2b42b799983271323b5633669
Pérez <lmperez@sice-outsourcing.com>
2024-02-22 12:49:01 +0100

    dependencias

--- src/main/java/org/babel/Main.java
+++ src/main/java/org/babel/Main.java
@@ -1,7 +1,26 @@
 package org.babel;
 
+import com.google.gson.Gson;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Crear un objeto Java
+        Persona persona = new Persona("Juan", 25);
+
+        // Convertir objeto Java a JSON
+        Gson gson = new Gson();
+        String json = gson.toJson(persona);
+        System.out.println("Objeto Java convertido a JSON:");
+        System.out.println(json);
+
+        // Convertir JSON a objeto Java
+        String jsonPersona = "{\"nombre\":\"María\",\"edad\":30}";
+        Persona personaDesdeJson = gson.fromJson(jsonPersona, Persona.class);
+        System.out.println("\nJSON convertido a objeto Java:");
+        System.out.println("Nombre: " + personaDesdeJson.getNombre());
+        System.out.println("Edad: " + personaDesdeJson.getEdad());
     }
+
+
 }
